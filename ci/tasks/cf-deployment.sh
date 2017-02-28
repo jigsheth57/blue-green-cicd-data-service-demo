5ac00cddae0220d339b8259d8ccbafc30158a379
Jignesh Sheth <jsheth@pivotal.io>
2017-02-27 18:20:27 -0600

    update

--- ci/tasks/cf-deployment.sh
+++ ci/tasks/cf-deployment.sh
@@ -16,7 +16,7 @@ if [ ! -z "$DEPLOYED_VERSION" -a "$DEPLOYED_VERSION" == "blue" ]; then
 fi
 # push a new version and map the route
 cf cs cleardb spark p-mysql
-cf cs p-rabbitmq standard p-rabbitmq
+cf cs cloudamqp lemur p-rabbitmq
 # cf cs p-mysql 100mb p-mysql
 # cf cs p-rabbitmq standard p-rabbitmq
 cf p "$CF_APP_NAME-$CURRENT_VERSION" -p lab-release/contactDataService-*.jar -f lab-repo/ci/tasks/manifest.yml
